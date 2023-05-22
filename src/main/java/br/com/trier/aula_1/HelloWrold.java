da7dcb62c25e381bbe5faeaab396ec68b1deecbe
renato nandi <renato.nandi@gmail.com>
2023-05-22 15:51:04 -0300

    segundo commit

--- src/main/java/br/com/trier/aula_1/HelloWrold.java
+++ src/main/java/br/com/trier/aula_1/HelloWrold.java
@@ -2,7 +2,8 @@ package br.com.trier.aula_1;
 
 public class HelloWrold {
     public static void main(String[] args) {
-        System.out.println("Ola mundo");
+        System.out.println("Ola mund!");
+        System.out.println("Segundo Commit");
         
     }
 

a4cf97b3cc70d05e101cfe0c71dbdefd060fcad9
Renato Nandi <renato.nandi@gmail.com>
2023-05-23 21:25:02 -0300

    Retirada de metodo não funcional da classe util

--- src/main/java/br/com/trier/aula_2/carros/Util.java
+++ src/main/java/br/com/trier/aula_2/carros/Util.java
@@ -9,23 +9,6 @@ public class Util {
 				+ "3 - Listar carros por marca\n" + "4 - Listar carros por cor\n\n" + "5 - Sair";
 		return Integer.parseInt(JOptionPane.showInputDialog(menu));
 	}
-	public static void verificaMarca() {
-		Carro carro = new Carro();
-		boolean validadorMarca = false;
-		while (!validadorMarca) {
-			try {
-				String marcaDigitada = JOptionPane.showInputDialog("Digite a marca do carro").toUpperCase();
-				if (marcaDigitada.isEmpty()) {
-					throw new Exception("A marca do carro deve ser preenchida ");
-				}
-				carro.marca = marcaDigitada;
-
-			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, e);
-				continue;
-			}
-			validadorMarca = true;
-		}
-	}
+	
 
 }

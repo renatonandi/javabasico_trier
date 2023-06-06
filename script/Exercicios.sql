c4b973ecfb3abb4b203ef4936c53c0fcf6f835a0
renato nandi <renato.nandi@gmail.com>
2023-06-06 17:27:18 -0300

    Commit final da aula de banco de dados

--- script/Exercicios.sql
+++ script/Exercicios.sql
@@ -32,50 +32,75 @@ inner join cargo ca on ca.id = c.cargo and ca.nome = 'Prefeito'order by ci.nome,
 
 -- 11
 select c.nome from candidato c 
-inner join cidade ci on ci.id = c.cidade 
-inner join cargo ca on ca.id = c.cargo  and ca.nome = 'Prefeito' and ci.nome = 'TUBARÃO' order by c.nome;
+inner join cidade ci on ci.id = c.cidade and ci.nome = 'TUBARÃO'
+inner join cargo ca on ca.id = c.cargo  and ca.nome = 'Prefeito' order by c.nome;
 
 -- 12
-select c.nome from candidato c inner join cidade ci on ci.id = c.cidade inner join cargo ca on ca.id = c.cargo  and ca.nome = 'Prefeito' and ci.qt_eleitores = (select max(qt_eleitores) from cidade);
+select c.nome from candidato c 
+inner join cidade ci on ci.id = c.cidade and ci.qt_eleitores = (select max(qt_eleitores) from cidade)
+inner join cargo ca on ca.id = c.cargo  and ca.nome = 'Prefeito';
 
 -- 13
-select count(*) from candidato c inner join cidade ci on ci.id = c.cidade inner join cargo ca on ca.id = c.cargo and ci.nome = 'TUBARÃO' and ca.nome = 'Vereador';
+select count(*) from candidato c 
+inner join cidade ci on ci.id = c.cidade and ci.nome = 'TUBARÃO' 
+inner join cargo ca on ca.id = c.cargo and ca.nome = 'Vereador';
 
 -- 14
-select (select count(*) from candidato ), ci.nome from candidato c inner join cidade ci on ci.id = c.cidade inner join cargo ca on ca.id = c.cargo and ca.nome = 'Vereador';
+select ci.nome, count(*)from candidato c 
+inner join cidade ci on ci.id = c.cidade 
+inner join cargo ca on ca.id = c.cargo and ca.nome = 'Vereador' group by ci.nome order by ci.nome;
 
 -- 15
-select sum(vi.brancos) as brancos, sum(vi.nulos) as nulos from voto_invalido vi inner join cidade ci on ci.id = vi.cidade and ci.nome = 'TUBARÃO';
+select ca.nome, vi.brancos, vi.nulos from cargo ca 
+inner join voto_invalido vi on vi.cargo = ca.id 
+inner join cidade ci on ci.id = vi.cidade and ci.nome = 'TUBARÃO';
 
 -- 16
-select sum(vi.nulos + vi.brancos) from voto_invalido vi inner join cidade ci ON ci.id = vi.cidade inner join cargo ca on ca.id = vi.cargo and ci.nome = 'TUBARÃO' and ca.nome = 'Prefeito'; 
+select vi.nulos + vi.brancos from voto_invalido vi 
+inner join cidade ci ON ci.id = vi.cidade and ci.nome = 'TUBARÃO' 
+inner join cargo ca on ca.id = vi.cargo and ca.nome = 'Prefeito'; 
 
 -- 17
-select c.nome, v.voto from voto v 
+select v.voto, c.nome from voto v 
 inner join candidato c on c.id = v.candidato 
-inner join cidade ci on ci.id = c.cidade 
-inner join cargo ca on ca.id = c.cargo and ca.nome = 'Prefeito' and ci.nome = 'TUBARÃO' order by v.voto desc;
+inner join cidade ci on ci.id = c.cidade and ci.nome = 'TUBARÃO' 
+inner join cargo ca on ca.id = c.cargo and ca.nome = 'Prefeito' order by v.voto desc;
 
 -- 18
-select c.nome, v.voto from voto v 
+select  v.voto, c.nome from voto v 
 inner join candidato c on c.id = v.candidato 
-inner join cidade ci on ci.id = c.cidade 
-inner join cargo ca on ca.id = c.cargo and ca.nome = 'Vereador' and ci.nome = 'TUBARÃO' order by v.voto desc;
+inner join cidade ci on ci.id = c.cidade and ci.nome = 'TUBARÃO'
+inner join cargo ca on ca.id = c.cargo and ca.nome = 'Vereador'  order by v.voto desc;
 
 -- 19
-select ci.nome, max(v.voto) from voto v 
+select  max(v.voto), ci.nome as NOME_CIDADE from voto v 
 inner join candidato c on c.id = v.candidato 
 inner join cidade ci on ci.id = c.cidade 
 inner join cargo ca on ca.id = c.cargo and ca.nome = 'Prefeito' group by ci.nome;
 
 -- 20
-select p.sigla, ci.nome, sum(v.voto) as quantidade_votos from voto v  
-inner join candidato c on c.id = v.candidato
-inner join partido p on p.id = c.partido 
-inner join cidade ci on ci.id = c.cidade group by p.sigla, ci.nome order by quantidade_votos desc; 
+select p.sigla, sum(v.voto) as quantidade_votos from partido p
+inner join candidato c on c.partido = p.id 
+inner join voto v on v.candidato = c.id 
+inner join cidade ci on ci.id = c.cidade and ci.nome = 'TUBARÃO' 
+group by p.sigla
+order by quantidade_votos desc; 
 
 --21
-select sum(v.voto + vi.brancos + vi.nulos) from voto v 
+select v.voto + vi.brancos + vi.nulos from voto v 
 inner join candidato c on c.id = v.candidato 
-inner join cargo ca on ca.id = c.cargo 
-inner join voto_invalido vi on vi.cargo = ca.id where ca.nome = 'Prefeito';
+inner join cargo on cargo.id = c.cargo and cargo.nome = 'Prefeito'
+inner join cidade ci on ci.id = c.cidade and ci.nome = 'TUBARÃO'
+inner join voto_invalido vi on vi.cidade = ci.id and vi.cargo = cargo.id; 
+
+
+
+--22
+select ci.qt_eleitores - (vi.brancos + vi.nulos + v.voto)  from cidade ci
+inner join candidato c on c.cidade = ci.id 
+inner join voto v on v.candidato = c.id 
+inner join voto_invalido vi on vi.cidade = ci.id 
+where ci.nome = 'TUBARÃO';
+
+--23
+select
